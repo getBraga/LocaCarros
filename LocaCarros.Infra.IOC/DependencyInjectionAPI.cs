e1a0d7d5a2de5fb23504a34390af5eeaaea7d11b
ysmael braga <ysmaelbraga01@gmail.com>
2025-07-04 22:13:58 -0300

    fazendo a injecao de dependencia IOC

---
+++ LocaCarros.Infra.IOC/DependencyInjectionAPI.cs
@@ -0,0 +1,28 @@
+﻿using LocaCarros.Domain.Interfaces;
+using LocaCarros.Infra.Data.Context;
+using LocaCarros.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocaCarros.Infra.IOC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IMarcaRepository, MarcaRepository>();
+            services.AddScoped<IModeloRepository, ModeloRepository>();
+            services.AddScoped<ICarroRepository, CarroRepository>();
+            services.AddScoped<IAluguelRepository, AluguelRepository>();
+            services.AddScoped<IVendaRepository, VendaRepository>();
+           
+            return services;
+        }
+
+    }
+}
