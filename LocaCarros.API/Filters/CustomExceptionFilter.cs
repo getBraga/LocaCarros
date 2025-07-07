79b121fff7a01029c990cccce547d890802d1645
ysmael braga <ysmaelbraga01@gmail.com>
2025-07-07 17:53:00 -0300

    retirado o stack trace das mensagens

---
+++ LocaCarros.API/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LocaCarros.API.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new JsonResult(new { error = context.Exception.Message })
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- LocaCarros.API/Program.cs
+++ LocaCarros.API/Program.cs
@@ -1,6 +1,9 @@
 ﻿
+using LocaCarros.API.Filters;
 using LocaCarros.Application.Mappings;
 using LocaCarros.Infra.IOC;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,24 +16,47 @@ builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddInfrastructureSwagger(); 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
+});
 var app = builder.Build();
 
 
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocaCarros.API v1");
-    c.RoutePrefix = string.Empty; // 👈 faz o Swagger ser carregado na raiz
+    c.RoutePrefix = string.Empty;
 });
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/error"); 
+    app.UseHsts();
+}
+    app.UseHttpsRedirection();
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest; // ou outro código
+        context.Response.ContentType = "application/json";
 
-app.UseHttpsRedirection();
+        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (contextFeature != null)
+        {
+            var errorMessage = contextFeature.Error.Message;
+            var result = JsonSerializer.Serialize(new { error = errorMessage });
+            await context.Response.WriteAsync(result);
+        }
+    });
+});
 
 app.UseAuthorization();
 
